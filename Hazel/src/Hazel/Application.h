c6aa917fcb18738b37b4f6f51398816f10ddefa5
krystian.rozwag <krystek_1@onet.eu>
2024-06-21 18:51:58 +0200

    Implemented own rendererapi to improve renderer flow and submission

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -8,6 +8,7 @@
 
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Buffer.h"
+#include "Renderer/VertexArray.h"
 
 namespace Hazel
 {
@@ -34,11 +35,12 @@ namespace Hazel
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
 
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+		std::shared_ptr<VertexArray> m_SquareVA;
+		std::shared_ptr<Shader> m_BlueShader;
+
 		static Application* s_Instance;
 	};
 
